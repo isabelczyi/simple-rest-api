3ba79dc5f6513a9cce54d1cf3ef6eae2f4156e2c
Isabel Cheung <isabelczyi@gmail.com>
2022-06-26 21:18:01 +1000

    made a service

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -7,4 +7,16 @@ export class MessagesService {
     // Don't do this in any real apps, dependency injection is used in Nest js usually this is just for exploration
     this.messagesRepo = new MessagesRepository();
   }
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
 }
