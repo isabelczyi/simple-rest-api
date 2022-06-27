5054bf64af62ff96bb617f122c72fa8c26d3221b
Isabel Cheung <isabelczyi@gmail.com>
2022-06-28 00:30:50 +1000

    starting dependency injection

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -2,10 +2,9 @@ import { MessagesRepository } from './messages.repository';
 
 export class MessagesService {
   messagesRepo: MessagesRepository;
-  constructor() {
-    // Service is creating its own dependencies
-    // Don't do this in any real apps, dependency injection is used in Nest js usually this is just for exploration
-    this.messagesRepo = new MessagesRepository();
+
+  constructor(messagesRepo: MessagesRepository) {
+    this.messagesRepo = messagesRepo;
   }
 
   findOne(id: string) {
